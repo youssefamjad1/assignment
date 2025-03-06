100fe33d9382ea8a22ffc7dcfaf99f7fa022c131
Youssef HIMMI <yhimmi1@gmail.com>
2025-03-06 02:49:29 +0100

    updating db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306 
 });
 
 db.connect((err) => {
